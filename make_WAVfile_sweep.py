3fb948bd3e6f4926ef92112151d43125d643cb9c
tbeleyur <tbeleyur@orn.mpg.de>
2018-07-02 10:28:48 +0300

    working version that saves a WAV file

--- make_WAVfile_sweep.py
+++ make_WAVfile_sweep.py
@@ -14,6 +14,31 @@ import matplotlib.pyplot as plt
 plt.rcParams['agg.path.chunksize'] = 10000
 import scipy.io.wavfile as WAV
 
-from speaker_sweep_playback import create_pbk_signals
+def create_pbk_signals():
+    '''Create the sync, trigger and sweep for playbacks that will create the
+    common positions for camera and mic arrays.
 
-_, _ , chirp = create_pbk_signals()
+    The sweeps are generated between 16-96 KHz, and are 6 ms long.
+
+    '''
+    fs = 192000
+
+    # create the sweep signal :
+    chirp_durn = 0.008
+    t_chirp = np.linspace(0,chirp_durn,int(fs*chirp_durn))
+    start_freq , end_freq = 16000, 96000
+
+    sweep = signal.chirp(t_chirp, start_freq, t_chirp[-1],end_freq,'hyperbolic')
+    sweep *= signal.tukey(sweep.size,0.8)
+    sweep = np.float32(sweep)
+    # a hack to compensate for the freq response of the speakers
+    # amplify the last part of the sweep 
+    sweep *= np.linspace(1,1.125,sweep.size)
+    # normalise so values remain between -1/1
+    sweep *= 1/np.max(sweep)
+
+    return(sweep)
+
+chirp = create_pbk_signals()
+
+WAV.write('chirp.WAV', 192000, chirp)
